ba8335c488bcef324df7e3b0e8141247db460d9e
Aiden Hammond <aiden@MacBookPro.attlocal.net>
2025-05-05 11:38:47 -0400

    removed profile path from user

--- src/main/java/uga/lroc/cs4370/models/User.java
+++ src/main/java/uga/lroc/cs4370/models/User.java
@@ -5,23 +5,11 @@ public class User {
     public final String userId;
     private final String firstName;
     private final String lastName;
-    private final String profileImagePath;
 
-    public User(String userId, String firstName, String lastName, String profileImagePath) {
+    public User(String userId, String firstName, String lastName) {
         this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.profileImagePath = profileImagePath;
-    }
-
-    public User(String userId, String firstName, String lastName) {
-        this(userId, firstName, lastName, getAvatarPath(userId));
-    }
-
-    private static String getAvatarPath(String userId) {
-        int fileNo = (userId.hashCode() % 20) + 1;
-        String avatarFileName = String.format("avatar_%d.png", fileNo);
-        return "/avatars/" + avatarFileName;
     }
 
     public String getUserId() {
@@ -36,7 +24,4 @@ public class User {
         return lastName;
     }
 
-    public String getProfileImagePath() {
-        return profileImagePath;
-    }
 }
